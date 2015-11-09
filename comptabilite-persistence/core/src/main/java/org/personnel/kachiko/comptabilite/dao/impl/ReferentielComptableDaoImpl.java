8ad8ee8b58a5fdc21695e0900dcb6e269489da3d
Kachiko_2 <Kachiko_2@192.168.1.13>
2015-11-09 05:44:23 +0100

    Ajout de la recupération des listes de comptes et de regroupement

--- comptabilite-persistence/core/src/main/java/org/personnel/kachiko/comptabilite/dao/impl/ReferentielComptableDaoImpl.java
+++ comptabilite-persistence/core/src/main/java/org/personnel/kachiko/comptabilite/dao/impl/ReferentielComptableDaoImpl.java
@@ -10,8 +10,11 @@ import javax.persistence.Query;
 import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
 import org.personnel.kachiko.comptabilite.entities.CompteComptableEntity;
+import org.personnel.kachiko.comptabilite.entities.RegroupementCompteEntity;
 import org.personnel.kachiko.comptabilite.persistence.dao.ReferentielComptableDao;
 import org.personnel.kachiko.comptabilite.persistence.dto.CompteComptableDto;
+import org.personnel.kachiko.comptabilite.persistence.dto.RegroupementCompteDto;
+import org.personnel.kachiko.framework.utils.DozerUtil;
 
 @Stateful
 public class ReferentielComptableDaoImpl implements ReferentielComptableDao {
@@ -22,12 +25,19 @@ public class ReferentielComptableDaoImpl implements ReferentielComptableDao {
 	Mapper mapper = new DozerBeanMapper();
 	
 	@SuppressWarnings("unchecked")
-	@Override
 	public List<CompteComptableDto> getListCompteComptable() {
 		List<CompteComptableEntity> listeCompte;
 		Query qry = em.createNamedQuery("Compte_Comptable.findAll");
 		listeCompte = qry.getResultList();
-		return null;
+		return DozerUtil.map(listeCompte, CompteComptableDto.class);
+	}
+
+	@Override
+	public List<RegroupementCompteDto> getListRegroupement() {
+		List<RegroupementCompteEntity> listeRegroupement;
+		Query qry = em.createNamedQuery("Regroupement_Compte.findAll");
+		listeRegroupement = qry.getResultList();
+		return DozerUtil.map(listeRegroupement, RegroupementCompteDto.class);
 	}
 	
 }
